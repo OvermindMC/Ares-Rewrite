e5bd2c32730e2031517fa694d010d43d6c389089
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-03-27 21:06:47 +0000

    Update Mem.cpp

--- Mem/Mem.cpp
+++ Mem/Mem.cpp
@@ -1,1 +1,10 @@
 #include "Mem.h"
+
+auto Mem::getDll(void) -> HMODULE {
+
+    MEMORY_BASIC_INFORMATION info;
+    size_t len = VirtualQueryEx(GetCurrentProcess(), (void*)getDll, &info, sizeof(info));
+    assert(len == sizeof(info));
+    return len ? (HMODULE)info.AllocationBase : NULL;
+
+};
