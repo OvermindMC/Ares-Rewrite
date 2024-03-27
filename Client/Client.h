64aa9d1ace073de1d26fcb8c379948cb50821ff4
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-03-27 21:22:41 +0000

    Update Client.h

--- Client/Client.h
+++ Client/Client.h
@@ -5,16 +5,30 @@
 
 #include "Version/Version.h"
 
+class Manager; /* Forward Declaration */
+
 class Client {
+/* Constructor, Deconstructor */
+
 public:
-    Client(const char* client_name) : name(client_name ? client_name : "") {}
+    Client(const char* client_name) : name(client_name ? client_name : "Client") {}
     ~Client(void) {};
+    
+/* Client Name, Client Version */
+
 public:
     auto getName(void) -> std::string;
     auto getVersion(void) -> Version;
 private:
     std::string name;
     Version version = Version(1, 0, 0);
+
+/* Runtime */
+
 private:
     bool isRunning = false;
+    Manager* mgr = nullptr;
+
+public:
+    auto init(void) -> void;
 };
