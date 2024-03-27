2e19c6f7a03ab8dff08aa67a0671bd16781c5c91
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-03-27 21:22:43 +0000

    Update Client.cpp

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,4 +1,5 @@
 #include "Client.h"
+#include "Manager/Manager.h"
 
 auto Client::getName(void) -> std::string {
 
@@ -9,5 +10,14 @@ auto Client::getName(void) -> std::string {
 auto Client::getVersion(void) -> Version {
 
     return this->version;
+
+};
+
+auto Client::init(void) -> void {
+
+    if(this->isRunning)
+        return;
+    
+    this->mgr = new Manager(this); /* Create new Manager instance */
 
 };
