b8dac06106e54dd37ab9d1fffff0238dbc040bd6
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-03-27 20:30:16 +0000

    Update Client.h

--- Client/Client.h
+++ Client/Client.h
@@ -5,5 +5,10 @@
 
 class Client {
 public:
-    //
+    Client(const char* client_name) : name(client_name ? client_name : "") {}
+    ~Client(void) {};
+public:
+    auto getName(void) -> std::string_view;
+private:
+    std::string_view name;
 };
