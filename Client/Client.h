dda49391767818e4e8acc2edfe5fd43e83209676
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-03-27 20:59:20 +0000

    Update Client.h

--- Client/Client.h
+++ Client/Client.h
@@ -3,12 +3,18 @@
 #include "../Mem/Mem.h"
 #include "../Utils/Utils.h"
 
+#include "Version/Version.h"
+
 class Client {
 public:
     Client(const char* client_name) : name(client_name ? client_name : "") {}
     ~Client(void) {};
 public:
-    auto getName(void) -> std::string_view;
+    auto getName(void) -> std::string;
+    auto getVersion(void) -> Version;
 private:
-    std::string_view name;
+    std::string name;
+    Version version = Version(1, 0, 0);
+private:
+    bool isRunning = false;
 };
