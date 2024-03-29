d1e677e87524bc022cb4c1f69cbda301ea16eaa0
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-03-29 08:59:05 +0000

    Update Manager.cpp

--- Client/Manager/Manager.cpp
+++ Client/Manager/Manager.cpp
@@ -1,7 +1,20 @@
 #include "Manager.h"
 
+Manager::Manager(Client* client_raw_ptr) : client(client_raw_ptr) {};
+
+Manager::~Manager(void) {
+
+    //
+
+};
+
 auto Manager::init(void) -> void {
 
     Debugger::log("Initialized Manager");
+
+    std::ostringstream o;
+    o << std::hex << this->client;
+
+    Debugger::log(o.str());
 
 };
