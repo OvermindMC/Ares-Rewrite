424cc31a0a3cbd7521b265c550e34d5ceb1889a4
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-03-29 13:04:37 +0000

    Update Player.h

--- SDK/Classes/Entity/Player.h
+++ SDK/Classes/Entity/Player.h
@@ -5,4 +5,8 @@
 class Player : public Actor {
 private:
     virtual ~Player(void);
+public:
+    virtual auto knockback(class Actor*, int, float, float, float, float, float) -> void;
+    virtual auto spawnAnim(void) -> void;
+    virtual auto setSprinting(bool) -> void;
 };
