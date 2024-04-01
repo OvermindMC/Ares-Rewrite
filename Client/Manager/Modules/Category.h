419a6619087d5e4b2fd1bcc933f607980e07e140
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-04-01 02:49:14 +0100

    Create Category.h

---
+++ Client/Manager/Modules/Category.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "../Manager.h"
+
+/* Forward Declarations */
+class Module;
+
+/* Category Type */
+enum class CategoryType {
+    COMBAT = 1, MOVE = 2, RENDER = 3, WORLD = 4, PLAYER = 5, MISC = 6
+};
+
+/* Category Class */
+class Category {
+public:    
+
+/* Constructor, Deconstructor */
+
+public:
+    Category(Manager* manager_raw_ptr, CategoryType category_type);
+    ~Category(void);
+
+/* Attributes for Category */
+private:
+    std::vector<Module*> modules;
+    Manager* mgr = nullptr;
+    CategoryType type;
+
+/* Methods for Runtime */
+public:
+    auto getType(void) -> CategoryType;
+    auto getName(void) -> std::string;
+public:
+    auto getModules(void) -> std::vector<Module*>;
+    auto addModule(Module* module_raw_ptr) -> bool;
+};
