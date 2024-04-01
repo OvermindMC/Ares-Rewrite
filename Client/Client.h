89c7475ea44a4b9dc635de67b4a602b87c69b4d4
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-04-01 02:55:37 +0100

    Update Client.h

--- Client/Client.h
+++ Client/Client.h
@@ -26,13 +26,16 @@ private:
     std::string name;
     Version version = Version(1, 0, 0);
 
-/* Runtime */
+/* Runtime Attributes */
 
 private:
     bool _isRunning = false;
     Manager* mgr = nullptr;
 
+/* Runtime Methods */
 public:
     auto init(void) -> void;
+public:
     auto isRunning(void) -> bool;
+    auto stop(void) -> void;
 };
