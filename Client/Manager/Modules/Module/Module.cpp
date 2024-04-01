f191441188cc05be92a33cea0bab7e223de18e02
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-04-01 03:35:06 +0100

    Update Module.cpp

--- Client/Manager/Modules/Module/Module.cpp
+++ Client/Manager/Modules/Module/Module.cpp
@@ -12,3 +12,49 @@ Module::~Module(void) {
     //
 
 };
+
+auto Module::baseTick(void) -> void {
+
+    if(this->state.first != this->state.second) {
+
+        this->state.second = this->state.first;
+
+        if(this->state.first) {
+            //
+        } else {
+            //
+        };
+
+    };
+
+};
+
+auto Module::getState(void) -> bool {
+
+    return this->state.first;
+
+};
+
+auto Module::toggleState(void) -> void {
+
+    this->state.first = !this->state.first;
+
+};
+
+auto Module::setState(bool module_state) -> void {
+
+    this->state.first = module_state;
+
+};
+
+auto Module::getBind(void) -> uint64_t {
+
+    return this->bindKey;
+
+};
+
+auto Module::setBind(uint64_t module_bind) -> void {
+
+    this->bindKey = module_bind;
+
+};
