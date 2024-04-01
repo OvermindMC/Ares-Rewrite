d9ab0d3f84b2f41a4d74c33e3444c89ae20ff8db
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-04-01 03:21:12 +0100

    Create Module.cpp

---
+++ Client/Manager/Modules/Module/Module.cpp
@@ -0,0 +1,13 @@
+#include "Module.h"
+
+Module::Module(Manager* manager_raw_ptr, CategoryType category_type, std::string module_name, std::string module_description, uint64_t module_bind) : name(module_name), description(module_description), bindKey(module_bind) {
+
+    this->category = manager_raw_ptr->getCategory(category_type);
+
+};
+
+Module::~Module(void) {
+
+    //
+
+};
