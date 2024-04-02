3972ae0b81a96674f2c8fa279b70aa04f4d2c63a
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-04-02 21:45:45 +0100

    Update TestMod.cpp

--- Client/Manager/Modules/Module/Misc/TestMod.cpp
+++ Client/Manager/Modules/Module/Misc/TestMod.cpp
@@ -4,4 +4,21 @@ TestMod::TestMod(Manager* mgr) : Module(mgr, CategoryType::MISC, "Test Module", 
 
     this->setState(true);
 
+    this->getEventDispatcher()->registerEvent(
+        EventType::Module_Tick, EventDispatcher::EventPriority::Low, std::function<void(void)>(
+            [&]() -> void {
+                Debugger::log("Tick!");
+                this->setState(false);
+            }
+        )
+    );
+
+    this->getEventDispatcher()->registerEvent(
+        EventType::Module_Toggle, EventDispatcher::EventPriority::Low, std::function<void(void)>(
+            [&]() -> void {
+                Debugger::log(this->name + " " + (this->getState() ? "Enabled!" : "Disabled!"));
+            }
+        )
+    );
+
 };
