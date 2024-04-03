ff66649d0cf1b014a99f92a45d85c3ba038c07cf
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-04-03 05:05:13 +0100

    Update Module.cpp

--- Client/Manager/Modules/Module/Module.cpp
+++ Client/Manager/Modules/Module/Module.cpp
@@ -3,9 +3,9 @@
 Module::Module(Manager* manager_raw_ptr, CategoryType category_type, std::string module_name, std::string module_description, uint64_t module_bind) : name(module_name), description(module_description), bindKey(module_bind) {
 
     this->category_raw_ptr = manager_raw_ptr->getCategory(category_type);
-    this->category_raw_ptr->addModule(this);
-
     this->event_dispatcher_raw_ptr = new EventDispatcher();
+    
+    this->category_raw_ptr->addModule(this);
 
 };
 
