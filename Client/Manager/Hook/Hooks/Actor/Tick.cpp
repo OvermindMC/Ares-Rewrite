e1b2ea96fc3d8b205b9b4d52e2ce9806ca702fc8
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-04-10 03:52:23 +0100

    Update Tick.cpp

--- Client/Manager/Hook/Hooks/Actor/Tick.cpp
+++ Client/Manager/Hook/Hooks/Actor/Tick.cpp
@@ -3,8 +3,16 @@
 Actor_TickHook::Actor_TickHook(Manager* mgr) : Hook<void, Actor*>(mgr, "Actor_Tick", (void*)mgr->getSig<uintptr_t**>("Actor_VTable")[29],
     [&](Actor* entity) -> void {
         
-        //auto runtimeId = entity->getRuntimeID();
-        //
+        auto typeId = entity->getEntityTypeId();
+        auto runtimeId = entity->getRuntimeID();
+        
+        if(typeId == 63 || ( EntityUtils::isHostile(typeId) || EntityUtils::isPassive(typeId) )) {
+            std::lock_guard<std::mutex> lock(entityMapMutex);
+            auto& entityMap = this->mgr->entityMap;
+
+            if(!entityMap.contains(runtimeId))
+                entityMap.emplace(runtimeId, entity);
+        };
         
         return this->_Func(entity);
     }
