bdacf44360048a61c84d916fc7b3b42813a2c233
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-04-10 08:46:34 +0100

    Update Manager.h

--- Client/Manager/Manager.h
+++ Client/Manager/Manager.h
@@ -44,6 +44,7 @@ public:
     };
 public:
     auto registerHook(void* hook_raw_ptr) -> bool;
+    auto getHookRaw(std::string) -> void*;
 public:
     auto getCategory(CategoryType category_type) -> Category*;
     auto getSortedEvents(void) -> std::map<EventType, std::vector<std::pair<EventDispatcher::EventPriority, void*>>>;
