e6978f970f7cbbff9e420494b8b58721b79e16e7
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-04-10 04:56:42 +0100

    Level tick hook

---
+++ Client/Manager/Hook/Hooks/Level/Tick.cpp
@@ -0,0 +1,23 @@
+#include "Tick.h"
+#include "../Actor/Tick.h"
+
+Level_TickHook::Level_TickHook(Manager* mgr) : Hook<void, Level*>(mgr, "Level_Tick", mgr->getSig<void**>("Level_VTable")[99],
+    [&](Level* level) -> void {
+        
+        std::lock_guard<std::mutex> lock(entityMapMutex);
+        auto& entityMap = this->mgr->entityMap;
+
+        for(auto iter = entityMap.begin(); iter != entityMap.end();) {
+            auto [ runtimeId, entity ] = *iter;
+            if(!entity || !entity->isAlive()) {
+                iter = entityMap.erase(iter);
+            } else {
+                ++iter;
+            };
+        };
+
+        this->mgr->dispatchEvent<EventType::Level_Tick, Level*>(level);
+        return this->_Func(level);
+
+    }
+) {};
