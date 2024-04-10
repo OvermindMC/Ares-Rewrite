1fb568dc2107344dbb4dabaaf650d17eed9f54d4
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-04-10 04:56:07 +0100

    Update Component.h

--- SDK/Classes/Entity/Component/Component.h
+++ SDK/Classes/Entity/Component/Component.h
@@ -1,13 +1,68 @@
 #pragma once
 
-struct RuntimeIDComponent {
+#include "Flags.h"
+
+struct RuntimeIDComponent
+{
     unsigned long long runtimeId;
 };
 
 struct ActorGameTypeComponent {
-    enum GameType gameType;
+    enum GameType type;
 };
 
 struct ActorTypeComponent {
     uint8_t typeId;
+};
+
+struct MaxAutoStepComponent {
+    float maxAutoStep;
+};
+
+struct FlySpeedComponent {
+    float flightSpeed;
+};
+
+struct MovementSpeedComponent {
+    float speed;
+};
+
+struct SwimSpeedMultiplierComponent {
+    float speed;
+};
+
+struct RenderPositionComponent {
+    Vec3<float> renderPos;
+};
+
+struct ActorSetPositionRequestComponent {
+    Vec3<float> position;
+};
+
+struct StateVectorComponent {
+    Vec3<float> position;
+    Vec3<float> oldPosition;
+    Vec3<float> positionDelta;
+};
+
+struct ActorRotationComponent {
+    Vec2<float> rotation;
+    Vec2<float> rotationPrev;
+};
+
+struct AABBShapeComponent {
+    AABB<float> AABB;
+    Vec2<float> radius;
+};
+
+struct AbilitiesComponent {
+    //
+};
+
+struct BlockMovementSlowdownMultiplierComponent {
+    //
+};
+
+struct ItemInUseComponent {
+    //
 };
