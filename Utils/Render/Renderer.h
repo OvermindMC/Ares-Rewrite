6fcaac3989ae9886826a25ed590731e686d9e1a6
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-04-10 06:12:34 +0100

    Update Renderer.h

--- Utils/Render/Renderer.h
+++ Utils/Render/Renderer.h
@@ -5,9 +5,14 @@
 #include "ImGui/imgui_impl_dx11.h"
 #include "ImGui/imgui_impl_win32.h"
 
+#include <string>
+
 class Renderer {
 private:
     static bool init;
+    static ImDrawList* drawList;
 public:
     static auto getSCVtable(void) -> unsigned long long*;
+public:
+    static auto drawText(ImVec2, std::string, float, ImColor) -> void;
 };
