486dc9ca1922983c13fb07f6b2cc9a9d4a6fe852
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-04-13 09:57:50 +0100

    Update Uninject.cpp

--- Client/Manager/Modules/Module/Misc/Uninject.cpp
+++ Client/Manager/Modules/Module/Misc/Uninject.cpp
@@ -1,6 +1,6 @@
 #include "Uninject.h"
 
-Uninject::Uninject(Manager* mgr) : Module(mgr, CategoryType::MISC, "Uninject Client from Game") {
+Uninject::Uninject(Manager* mgr) : Module(mgr, CategoryType::MISC, "Uninject") {
 
     this->getEventDispatcher()->registerEvent(
         EventType::Module_Toggle, EventDispatcher::EventPriority::Low, std::function<void(void)>(
