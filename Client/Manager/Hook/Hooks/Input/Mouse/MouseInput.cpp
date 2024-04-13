3fa7825b5baf313ad35e5585b47cc4f0ba044880
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-04-13 09:15:04 +0100

    Update MouseInput.cpp

--- Client/Manager/Hook/Hooks/Input/Mouse/MouseInput.cpp
+++ Client/Manager/Hook/Hooks/Input/Mouse/MouseInput.cpp
@@ -4,6 +4,26 @@ MouseInput_Hook::MouseInput_Hook(Manager* mgr) : Hook<void, void*, char, bool, s
     [&](void* p1, char action, bool isDown, short x, short y, short deltaX, short deltaY, bool isScroll) -> void {
         
         this->mgr->dispatchEvent<EventType::MouseInput, uint64_t, bool>(action, isDown, Vec2<float>(x, y));
+
+        if(ImGui::GetCurrentContext()) {
+            auto& io = ImGui::GetIO();
+            auto instance = MC::getClientInstance();
+
+            if(instance) {
+                auto mousePos = ImVec2(instance->mousePos._x, instance->mousePos._y);
+
+                io.MousePos = mousePos;
+                io.MousePosPrev = mousePos;
+
+                if(action > 0 && action < 3) {
+                    io.MouseDown[0] = isDown;
+                    io.AddMouseButtonEvent(action, isDown);
+                };
+
+                //io.AddMousePosEvent(mousePos.x, mousePos.y);
+            };
+        };
+
         return this->_Func(p1, action, isDown, x, y, deltaX, deltaY, isScroll);
 
     }
