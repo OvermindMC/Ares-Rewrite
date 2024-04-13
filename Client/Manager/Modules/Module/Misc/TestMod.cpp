ef171c8814dfcf0fc074241e03f7b22f11ef25b2
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-04-13 07:46:28 +0100

    Update TestMod.cpp

--- Client/Manager/Modules/Module/Misc/TestMod.cpp
+++ Client/Manager/Modules/Module/Misc/TestMod.cpp
@@ -7,57 +7,70 @@ TestMod::TestMod(Manager* mgr) : Module(mgr, CategoryType::MISC, "Test Module", 
     this->getEventDispatcher()->registerEvent(
         EventType::Present_Tick, EventDispatcher::EventPriority::Highest, std::function<void(void)>(
             [&](void) -> void {
-
-                static auto frameA = LiteRender::Frame({
-                    new LiteRender::Container(
-                        new LiteRender::Text(
-                            "Text A"
-                        )
-                    ),
-                    new LiteRender::Container(
-                        new LiteRender::Text(
-                            "Text B"
-                        )
-                    ),
-                    new LiteRender::Container(
-                        new LiteRender::Text(
-                            "Text C"
-                        )
-                    )
-                });
-
-                frameA.setStylesAll(LiteRender::Element::ElementStyle(
-                    ImColor(24.f, 201.f, 92.f), ImColor(252.f, 130.f, 8.f)
-                ));
-
-                frameA.setPos(100.f, 100.f);
-                frameA.render();
-
-                static auto frameB = LiteRender::Frame({
-                    new LiteRender::Container(
-                        new LiteRender::Text(
-                            "Text D"
-                        )
-                    ),
-                    new LiteRender::Container(
-                        new LiteRender::Text(
-                            "Text E"
-                        )
-                    ),
-                    new LiteRender::Container(
-                        new LiteRender::Text(
-                            "Text F"
-                        )
-                    )
-                });
-
-                frameB.setStylesAll(LiteRender::Element::ElementStyle(
-                    ImColor(8.f, 157.f, 252.f), ImColor(252.f, 130.f, 8.f)
-                ));
+                static auto window = LiteRender::Window(
+                    "Test Window", 18.f, {
+                        new LiteRender::Frame({
+                            new LiteRender::Container(
+                                new LiteRender::Text(
+                                    "Text A"
+                                )
+                            ),
+                            new LiteRender::Container(
+                                new LiteRender::Text(
+                                    "Text B"
+                                )
+                            ),
+                            new LiteRender::Container(
+                                new LiteRender::Button(
+                                    "Button 1", ImColor(255.f, 255.f, 255.f),
+                                    [=](char action, bool isDown) -> void {
+                                        //
+                                    }
+                                )
+                            ),
+                            new LiteRender::Container(
+                                new LiteRender::Button(
+                                    "Button 2", ImColor(255.f, 255.f, 255.f),
+                                    [=](char action, bool isDown) -> void {
+                                        //
+                                    }
+                                )
+                            )
+                        }),
+                        new LiteRender::Frame({
+                            new LiteRender::Container(
+                                new LiteRender::Button(
+                                    "Button", ImColor(255.f, 255.f, 255.f),
+                                    [=](char action, bool isDown) -> void {
+                                        //
+                                    }
+                                )
+                            ),
+                            new LiteRender::Container(
+                                new LiteRender::Text(
+                                    "Text"
+                                )
+                            )
+                        })
+                    }
+                );
                 
-                frameB.setPos(frameA.getBounds().z + frameB.getSpace(), 100.f);
-                frameB.render();
+                window.setStylesFor(
+                    LiteRender::Element::ElementType::Button,
+                    LiteRender::Element::ElementStyle(
+                        ImColor(122.f, 31.f, 196.f), ImColor(54.f, 52.f, 51.f)
+                    )
+                );
+                
+                window.setWindowStyles(
+                    LiteRender::Element::ElementStyle(
+                        ImColor(100.f, 110.f, 178.f), ImColor(21.f, 21.f, 21.f)
+                    )
+                );
 
+                window.setExtraSpace(6.f);
+                window.setPos(100.f, 100.f);
+                window.render();
             }
         )
     );
