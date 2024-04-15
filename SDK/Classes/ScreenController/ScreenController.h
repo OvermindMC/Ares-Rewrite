ce03bece565911b9e147c906c8c4caa7af6033c0
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-04-15 19:56:34 +0100

    Update ScreenController.h

--- SDK/Classes/ScreenController/ScreenController.h
+++ SDK/Classes/ScreenController/ScreenController.h
@@ -5,4 +5,9 @@
 class ScreenController {
 public:
     auto getName(void) -> std::string;
+public:
+    auto closeScreen(void) -> void;
+public:
+    auto handleDropItem(std::string container, int slot) -> void;
+    auto handleAutoPlace(std::string container, int slot) -> void;
 };
