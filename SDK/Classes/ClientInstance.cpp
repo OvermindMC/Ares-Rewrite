999612f75b8bd4370f5af7a60f14ea3fc01946c9
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-04-15 17:54:12 +0100

    Update ClientInstance.cpp

--- SDK/Classes/ClientInstance.cpp
+++ SDK/Classes/ClientInstance.cpp
@@ -5,3 +5,21 @@ auto ClientInstance::getLocalPlayer(void) -> Player* {
     return Mem::callVFunc<29, Player*>(this);
 
 };
+
+auto ClientInstance::getGuiData(void) -> GuiData* {
+
+    return *(GuiData**)((uintptr_t)(this) + 0x558);
+
+};
+
+auto ClientInstance::grabMouse(void) -> void {
+
+    return Mem::callVFunc<331, void>(this);
+
+};
+
+auto ClientInstance::releaseMouse(void) -> void {
+
+    return Mem::callVFunc<332, void>(this);
+
+};
