f4f184ea7c94a2392e66346b6c0557fcb655e5b3
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-04-15 17:54:10 +0100

    Update ClientInstance.h

--- SDK/Classes/ClientInstance.h
+++ SDK/Classes/ClientInstance.h
@@ -1,10 +1,15 @@
 #pragma once
 
 #include "Entity/Player.h"
+#include "GuiData/GuiData.h"
 
 class ClientInstance {
 public:
     auto getLocalPlayer(void) -> Player*;
+    auto getGuiData(void) -> GuiData*;
+public:
+    auto grabMouse(void) -> void;
+    auto releaseMouse(void) -> void;
 public:
     BUILD_ACCESS(Vec2<float>, mousePos, 0x498);
 };
