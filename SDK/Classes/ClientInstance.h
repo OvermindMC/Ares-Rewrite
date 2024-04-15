ff0bf147d2884b2cb3e1d69e8b9f5bbf0e4482f2
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-04-15 18:13:47 +0100

    Update ClientInstance.h

--- SDK/Classes/ClientInstance.h
+++ SDK/Classes/ClientInstance.h
@@ -2,12 +2,14 @@
 
 #include "Entity/Player.h"
 #include "GuiData/GuiData.h"
+#include "MinecraftGame/MinecraftGame.h"
 
 class ClientInstance {
 public:
     auto getLocalPlayer(void) -> Player*;
 public:
     auto getLevelRenderer(void) -> LevelRenderer*;
+    auto getMinecraftGame(void) -> MinecraftGame*;
     auto getGuiData(void) -> GuiData*;
 public:
     auto grabMouse(void) -> void;
