356c9f4d0ba20284007180b414220bfc60a586e2
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-04-15 18:05:10 +0100

    Create LevelRenderer.h

---
+++ SDK/Classes/Entity/Level/LevelRenderer/LevelRenderer.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "../../../../../Mem/Math/Vector.h"
+#include "../../../../../Mem/Macro/Class.h"
+
+class LevelRendererPlayer {
+public:
+    BUILD_ACCESS(Vec3<float>, eyePos, 0x5FC);
+};
+
+class LevelRenderer {
+public:
+    auto getRenderPlayer(void) -> LevelRendererPlayer* {
+        return *(LevelRendererPlayer**)((uintptr_t)(this) + 0x308);
+    };
+public:
+    AS_FIELD(LevelRendererPlayer*, renderPlayer, getRenderPlayer);
+};
