7a2d2e57c4d58bf1e3d122ea80c3f5888bddf248
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-04-15 20:37:27 +0100

    Update KeyInput.cpp

--- Client/Manager/Hook/Hooks/Input/Key/KeyInput.cpp
+++ Client/Manager/Hook/Hooks/Input/Key/KeyInput.cpp
@@ -1,7 +1,20 @@
 #include "KeyInput.h"
+#include "../../../../Modules/Module/Module.h"
 
 KeyInput_Hook::KeyInput_Hook(Manager* mgr) : Hook<void, unsigned long long, bool>(mgr, "Input_Key", mgr->getSig<void*>("KeyInput"),
     [&](unsigned long long key, bool isDown) -> void {
+
+        auto instance = MC::getClientInstance();
+        auto canUseKeys = ( instance ? instance->getLocalPlayer() ? instance->getScreenName().rfind("hud_screen") != std::string::npos : false : true );
+
+        if(canUseKeys && isDown) {
+            for(auto category : this->mgr->getCategories()) {
+                for(auto module : category->getModules()) {
+                    if(module->bindKey == key)
+                        module->toggleState();
+                };
+            };
+        };
         
         this->mgr->dispatchEvent<EventType::KeyInput, uint64_t, bool>(key, isDown);
         return this->_Func(key, isDown);
