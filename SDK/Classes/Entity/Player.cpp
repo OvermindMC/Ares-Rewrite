3c59e3ce4d4bdc72db0b06ba309299fcab2f0714
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-05-18 23:20:31 +0100

    Update Player.cpp

--- SDK/Classes/Entity/Player.cpp
+++ SDK/Classes/Entity/Player.cpp
@@ -5,3 +5,9 @@ auto Player::getGameMode(void) -> GameMode* {
     return *(GameMode**)((uintptr_t)(this) + 0xEB0);
 
 };
+
+auto Player::getSupplies(void) -> PlayerInventory* {
+
+    return *(PlayerInventory**)((uintptr_t)(this) + 0x788);
+
+};
