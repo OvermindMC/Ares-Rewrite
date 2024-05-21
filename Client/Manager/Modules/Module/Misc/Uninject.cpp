aaeca7c24efd53687dae7f056303f1ae5082ad8e
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-05-21 02:53:11 +0100

    Update Uninject.cpp

--- Client/Manager/Modules/Module/Misc/Uninject.cpp
+++ Client/Manager/Modules/Module/Misc/Uninject.cpp
@@ -1,6 +1,6 @@
 #include "Uninject.h"
 
-Uninject::Uninject(Manager* mgr) : Module(mgr, CategoryType::MISC, "Uninject") {
+Uninject::Uninject(Manager* mgr) : Module(mgr, CategoryType::MISC, "Uninject", "Remove the Client from the Game") {
 
     this->getEventDispatcher()->registerEvent(
         EventType::Module_Toggle, EventDispatcher::EventPriority::Low, std::function<void(void)>(
