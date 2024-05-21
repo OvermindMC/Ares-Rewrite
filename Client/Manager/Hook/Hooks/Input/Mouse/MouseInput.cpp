a28ec1234997a449bf55d49e6bbae673beb29c80
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-05-21 01:41:32 +0100

    Update MouseInput.cpp

--- Client/Manager/Hook/Hooks/Input/Mouse/MouseInput.cpp
+++ Client/Manager/Hook/Hooks/Input/Mouse/MouseInput.cpp
@@ -3,7 +3,7 @@
 MouseInput_Hook::MouseInput_Hook(Manager* mgr) : Hook<void, void*, char, bool, short, short, short, short, bool>(mgr, "Input_Mouse", mgr->getSig<void*>("MouseInput"),
     [&](void* p1, char action, bool isDown, short x, short y, short deltaX, short deltaY, bool isScroll) -> void {
         
-        this->mgr->dispatchEvent<EventType::MouseInput, uint64_t, bool>(action, isDown, Vec2<float>(x, y));
+        this->mgr->dispatchEvent<EventType::MouseInput, char, bool, Vec2<float>>(action, isDown, Vec2<float>(x, y));
 
         if(ImGui::GetCurrentContext()) {
             auto& io = ImGui::GetIO();
