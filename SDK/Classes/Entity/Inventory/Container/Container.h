008ffc4f2fa5a0e68251784be37bc6ca298837a1
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-05-23 05:43:49 +0100

    Update Container.h

--- SDK/Classes/Entity/Inventory/Container/Container.h
+++ SDK/Classes/Entity/Inventory/Container/Container.h
@@ -2,9 +2,13 @@
 
 #include "../../../../Mem/Mem.h"
 
+class ItemStack;
+
 class Container {
 private:
     virtual ~Container(void);
 public:
-    //
+    auto getItem(int slot) -> ItemStack* {
+        return Mem::callVFunc<5, ItemStack*, int>(this, slot);
+    };
 };
