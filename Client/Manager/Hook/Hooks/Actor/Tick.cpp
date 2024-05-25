93940268c5dfea60f584224c99eedf0f7f875df4
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-05-26 00:19:42 +0100

    Update Tick.cpp

--- Client/Manager/Hook/Hooks/Actor/Tick.cpp
+++ Client/Manager/Hook/Hooks/Actor/Tick.cpp
@@ -1,17 +1,19 @@
 #include "Tick.h"
 
-Actor_TickHook::Actor_TickHook(Manager* mgr) : Hook<void, Actor*>(mgr, "Actor_Tick", mgr->getSig<void**>("Actor_VTable")[29],
+Actor_TickHook::Actor_TickHook(Manager* mgr) : Hook<void, Actor*>(mgr, "Actor_Tick", mgr->getSig<void**>("Actor_VTable")[27],
     [&](Actor* entity) -> void {
         
-        auto typeId = entity->getEntityTypeId();
-        auto runtimeId = entity->getRuntimeID();
-        
-        if(typeId == 63 || ( EntityUtils::isHostile(typeId) || EntityUtils::isPassive(typeId) )) {
-            std::lock_guard<std::mutex> lock(entityMapMutex);
-            auto& entityMap = this->mgr->entityMap;
+        if(entity && entity->isAlive()) {
+            auto typeId = entity->getEntityTypeId();
+            auto runtimeId = entity->getRuntimeID();
+            
+            if(typeId == 63 || ( EntityUtils::isHostile(typeId) || EntityUtils::isPassive(typeId) )) {
+                std::lock_guard<std::mutex> lock(entityMapMutex);
+                auto& entityMap = this->mgr->entityMap;
 
-            if(!entityMap.contains(runtimeId))
-                entityMap.insert(std::pair<uint64_t, Actor*>(runtimeId, entity));
+                if(!entityMap.contains(runtimeId))
+                    entityMap.emplace(runtimeId, entity);
+            };
         };
         
         return this->_Func(entity);
