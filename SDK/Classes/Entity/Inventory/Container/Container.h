6673d61b6a02296631bd4c9c147a588c416b5817
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-05-25 16:22:16 +0100

    Update Container.h

--- SDK/Classes/Entity/Inventory/Container/Container.h
+++ SDK/Classes/Entity/Inventory/Container/Container.h
@@ -3,10 +3,22 @@
 #include "ItemStack/ItemStack.h"
 
 class Container {
-private:
-    virtual ~Container(void);
 public:
     auto getItem(int slot) -> ItemStack* {
         return Mem::callVFunc<5, ItemStack*, int>(this, slot);
+    };
+
+    auto getSlots(void) -> std::vector<ItemStack*> {
+        auto list = std::vector<ItemStack*>();
+
+        for(auto i = 0; i < this->getSize(); i++) {
+            list.push_back(this->getItem(i));
+        };
+
+        return list;
+    };
+public:
+    auto getSize(void) -> size_t {
+        return Mem::callVFunc<17, size_t>(this);
     };
 };
