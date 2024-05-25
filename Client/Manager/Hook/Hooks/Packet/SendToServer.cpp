73e29dce1b77ebb51e0c9899c0caa7044b4f5d39
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-05-25 23:40:30 +0100

    Update SendToServer.cpp

--- Client/Manager/Hook/Hooks/Packet/SendToServer.cpp
+++ Client/Manager/Hook/Hooks/Packet/SendToServer.cpp
@@ -1,9 +1,9 @@
 #include "SendToServer.h"
 
-SendToServer_Hook::SendToServer_Hook(Manager* mgr) : Hook<void, void*, void*>(mgr, "SendToServer", mgr->getSig<void**>("LoopbackPacketSender_VTable")[2],
-    [&](void* _this, void* packet) -> void {
+SendToServer_Hook::SendToServer_Hook(Manager* mgr) : Hook<void, LoopbackPacketSender*, Packet*>(mgr, "SendToServer", mgr->getSig<void**>("LoopbackPacketSender_VTable")[2],
+    [&](LoopbackPacketSender* _this, Packet* packet) -> void {
         
-        //this->mgr->dispatchEvent<EventType::ScreenControllerTick, ScreenController*>(screen_controller);
+        this->mgr->dispatchEvent<EventType::Packet_SendToServer, LoopbackPacketSender*, Packet*>(_this, packet);
 
         return this->_Func(_this, packet);
 
