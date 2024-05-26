787a542e4f52a7b4234af75e700c8afeb752b318
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-05-26 21:41:55 +0100

    Create MovePlayerPacket.h

---
+++ SDK/Classes/LoopbackPacketSender/Packet/MovePlayer/MovePlayerPacket.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "../Packet.h"
+
+class MovePlayerPacket : public Packet {
+public:
+    BUILD_ACCESS(uint64_t, runtimeId, 0x30);
+    BUILD_ACCESS(Vec3<float>, position, 0x38);
+    BUILD_ACCESS(Vec2<float>, bodyRot, 0x44);
+    BUILD_ACCESS(float, headYaw, 0x4C);
+    BUILD_ACCESS(bool, onGround, 0x51);
+    BUILD_ACCESS(uint64_t, ridingRuntimeId, 0x58);
+    BUILD_ACCESS(uint64_t, tick, 0x68);
+};
+
+template<PacketID>
+struct PacketTypeMap;
+
+template<>
+struct PacketTypeMap<PacketID::Move_Player> {
+    using type = MovePlayerPacket;
+};
