ee30fb77306564aa3abc9a253d7beecb9d014695
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-05-26 19:35:06 +0100

    Update TextPacket.cpp

--- SDK/Classes/LoopbackPacketSender/Packet/Text/TextPacket.cpp
+++ SDK/Classes/LoopbackPacketSender/Packet/Text/TextPacket.cpp
@@ -1,11 +1,1 @@
 #include "TextPacket.h"
-
-TextPacket::TextPacket(std::string message_contents, std::string message_author, TextPacketType text_type) {
-
-    std::memcpy(this, Packet::createPacket<PacketID::Text>().get(), sizeof(TextPacket));
-
-    this->type = text_type;
-    this->author = message_author;
-    this->message = message_contents;
-
-};
