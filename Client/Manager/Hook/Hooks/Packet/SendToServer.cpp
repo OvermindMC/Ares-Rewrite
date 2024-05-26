37659e8649e53a42b601d311ec8cd6dc9c0e6cf7
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-05-26 20:00:17 +0100

    Update SendToServer.cpp

--- Client/Manager/Hook/Hooks/Packet/SendToServer.cpp
+++ Client/Manager/Hook/Hooks/Packet/SendToServer.cpp
@@ -3,9 +3,12 @@
 SendToServer_Hook::SendToServer_Hook(Manager* mgr) : Hook<void, LoopbackPacketSender*, Packet*>(mgr, "SendToServer", mgr->getSig<void**>("LoopbackPacketSender_VTable")[2],
     [&](LoopbackPacketSender* _this, Packet* packet) -> void {
         
-        this->mgr->dispatchEvent<EventType::Packet_SendToServer, LoopbackPacketSender*, Packet*>(_this, packet);
+        bool cancel = false;
 
-        return this->_Func(_this, packet);
+        this->mgr->dispatchEvent<EventType::Packet_SendToServer, LoopbackPacketSender*, Packet*, bool*>(_this, packet, &cancel);
+
+        if(!cancel)
+            return this->_Func(_this, packet);
 
     }
 ) {};
