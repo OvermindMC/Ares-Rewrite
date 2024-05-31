9bb6f47baa43371503be8345171bb99ee8a56a11
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-05-31 07:49:26 +0100

    Update Level.h

--- SDK/Classes/Entity/Level/Level.h
+++ SDK/Classes/Entity/Level/Level.h
@@ -6,6 +6,9 @@ class Level {
 public:
     auto getEntityContextList(void) -> std::vector<struct EntityContext*>;
 public:
+    template<typename T>
+    auto getEntityComponents(void) -> std::vector<T*>;
+public:
     auto getRuntimeIds(void) -> std::vector<uint64_t>;
     auto getUniqueIds(void) -> std::vector<uint64_t>;
 public:
