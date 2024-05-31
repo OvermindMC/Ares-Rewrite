0563e8030c58a343689dae15782c7c68f2054616
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-05-31 10:45:16 +0100

    Update TestMod.cpp

--- Client/Manager/Modules/Module/Misc/TestMod.cpp
+++ Client/Manager/Modules/Module/Misc/TestMod.cpp
@@ -1,83 +1,12 @@
 #include "TestMod.h"
 
-template <typename T>
-class OwnerPtr {
-public:
-    std::shared_ptr<T> mHandle;
-
-    constexpr OwnerPtr() noexcept = default;
-
-    constexpr ~OwnerPtr() = default;
-
-    constexpr OwnerPtr(std::nullptr_t) noexcept {}
-
-    template <class Y>
-    constexpr OwnerPtr(std::shared_ptr<Y> const& ptr)
-        requires(std::convertible_to<Y*, T*>)
-    : mHandle(ptr) {}
-    template <class Y>
-    constexpr OwnerPtr(std::shared_ptr<Y>&& ptr) noexcept
-        requires(std::convertible_to<Y*, T*>)
-    : mHandle(std::move(ptr)) {}
-    template <class Y>
-    constexpr OwnerPtr(OwnerPtr<Y> const& other)
-        requires(std::convertible_to<Y*, T*>)
-    : mHandle(other.mHandle) {}
-    template <class Y>
-    constexpr OwnerPtr(OwnerPtr<Y>&& other) noexcept
-        requires(std::convertible_to<Y*, T*>)
-    : mHandle(std::move(other.mHandle)) {}
-
-    template <class Y>
-    constexpr OwnerPtr& operator=(std::shared_ptr<Y> const& other)
-        requires(std::convertible_to<Y*, T*>)
-    {
-        mHandle = other;
-        return *this;
-    }
-
-    template <class Y>
-    constexpr OwnerPtr& operator=(std::shared_ptr<Y>&& other) noexcept
-        requires(std::convertible_to<Y*, T*>)
-    {
-        mHandle = std::move(other);
-        return *this;
-    }
-
-    template <class Y>
-    constexpr OwnerPtr& operator=(OwnerPtr<Y> const& other)
-        requires(std::convertible_to<Y*, T*>)
-    {
-        if (this != &other) {
-            mHandle = other.mHandle;
-        }
-        return *this;
-    }
-
-    template <class Y>
-    constexpr OwnerPtr& operator=(OwnerPtr<Y>&& other) noexcept
-        requires(std::convertible_to<Y*, T*>)
-    {
-        if (this != &other) {
-            mHandle = std::move(other.mHandle);
-        }
-        return *this;
-    }
-
-    constexpr void reset() { mHandle.reset(); }
-
-    constexpr operator bool() const { return mHandle != nullptr; } // NOLINT
-
-    constexpr T* get() const { return mHandle.get(); }
-
-    constexpr T& operator*() const { return *get(); }
-
-    constexpr T* operator->() const { return get(); }
-};
-
 TestMod::TestMod(Manager* mgr) : Module(mgr, CategoryType::MISC, "Test Module", "For Developers") {
 
     this->setState(true);
+
+    for(auto i = 0; i < 3; i++) {
+        this->registerSetting<int>(std::to_string(i), &i);
+    };
 
     this->registerEvent<EventType::Module_Toggle, EventDispatcher::EventPriority::Low>(
         std::function<void(void)>(
@@ -90,11 +19,6 @@ TestMod::TestMod(Manager* mgr) : Module(mgr, CategoryType::MISC, "Test Module", 
 
                 if(!lp)
                     return;
-
-                auto level = lp->level;
-                auto msg = std::string("Entities: " + std::to_string(level->getEntities().size()));
-
-                lp->displayClientMessage(&msg);
             }
         )
     );
