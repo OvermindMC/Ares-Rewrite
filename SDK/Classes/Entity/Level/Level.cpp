448934a61d217fa4b34ccb2943a982ee958ed611
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-05-31 06:32:27 +0100

    Create Level.cpp

---
+++ SDK/Classes/Entity/Level/Level.cpp
@@ -0,0 +1,81 @@
+#include "../Actor.h"
+#include "../EntityContext.h"
+
+auto Level::getEntityContextList(void) -> std::vector<EntityContext*> {
+
+    return **(std::vector<EntityContext*>**)((uintptr_t)(this) + 0x1B78);
+
+};
+
+auto Level::getRuntimeIds(void) -> std::vector<uint64_t> {
+    auto results = std::vector<uint64_t>();
+
+    {
+        static auto sig = Mem::findSig("E8 ? ? ? ? BA 70 27 BA F0")->get(Signature::SearchType::Ref);
+
+        using AssureT = entt::basic_storage<RuntimeIDComponent, EntityId>* (__fastcall*)(entt::basic_registry<EntityId>&, entt::id_type);
+        auto assureF = (AssureT)(sig);
+
+        auto type = entt::type_id<RuntimeIDComponent>();
+        auto list = this->getEntityContextList();
+
+        for(auto it = list.begin(); it < list.end();) {
+            void* mRegistry = *it;
+            entt::basic_registry<EntityId>* registry = (entt::basic_registry<EntityId>*)list.at(std::distance(list.begin(), it + 1));
+            EntityId entityId = (EntityId&)list.at(std::distance(list.begin(), it + 2));
+
+            auto assure = assureF(*registry, type.hash());
+            auto runtimeId = assure->contains(entityId) ? assure->get(entityId).runtimeId : 0;
+
+            if(runtimeId)
+                results.push_back(runtimeId);
+
+            it += 4;
+        };
+    };
+
+    return results;
+};
+
+auto Level::getUniqueIds(void) -> std::vector<uint64_t> {
+    auto results = std::vector<uint64_t>();
+
+    {
+        static auto sig = Mem::findSig("E8 ? ? ? ? BA 70 27 BA F0")->get(Signature::SearchType::Ref);
+
+        using AssureT = entt::basic_storage<UniqueIDComponent, EntityId>* (__fastcall*)(entt::basic_registry<EntityId>&, entt::id_type);
+        auto assureF = (AssureT)(sig);
+
+        auto type = entt::type_id<UniqueIDComponent>();
+        auto list = this->getEntityContextList();
+
+        for(auto it = list.begin(); it < list.end();) {
+            void* mRegistry = *it;
+            entt::basic_registry<EntityId>* registry = (entt::basic_registry<EntityId>*)list.at(std::distance(list.begin(), it + 1));
+            EntityId entityId = (EntityId&)list.at(std::distance(list.begin(), it + 2));
+
+            auto assure = assureF(*registry, type.hash());
+            auto uniqueID = assure->contains(entityId) ? assure->get(entityId).uniqueId : 0;
+
+            if(uniqueID)
+                results.push_back(uniqueID);
+
+            it += 4;
+        };
+    };
+
+    return results;
+};
+
+auto Level::getEntities(void) -> std::vector<Actor*> {
+    auto results = std::vector<Actor*>();
+
+    for(auto runtimeId : this->getRuntimeIds()) {
+        auto entity = Mem::callVFunc<61, Actor*>(this);
+
+        if(entity)
+            results.push_back(entity);
+    };
+
+    return results;
+};
