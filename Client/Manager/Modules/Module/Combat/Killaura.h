ad46d6f91510539c13f89ca9545108fd7cca64c1
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-05-31 10:45:25 +0100

    Update Killaura.h

--- Client/Manager/Modules/Module/Combat/Killaura.h
+++ Client/Manager/Modules/Module/Combat/Killaura.h
@@ -5,4 +5,9 @@
 class Killaura : public Module {
 public:
     Killaura(Manager*);
+private:
+    float range = 12.f;
+    bool multi = true;
+private:
+    std::chrono::steady_clock::time_point timepoint = std::chrono::high_resolution_clock::now();
 };
