b60760abdd2f0401489f8308dbccf40437088808
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-06-01 15:24:57 +0100

    Update TestMod.cpp

--- Client/Manager/Modules/Module/Misc/TestMod.cpp
+++ Client/Manager/Modules/Module/Misc/TestMod.cpp
@@ -4,25 +4,6 @@ TestMod::TestMod(Manager* mgr) : Module(mgr, CategoryType::MISC, "Test Module", 
 
     this->setState(true);
 
-    for(auto i = 0; i < 3; i++) {
-        this->registerSetting<int>(std::to_string(i), &i);
-    };
-
-    this->registerEvent<EventType::Module_Toggle, EventDispatcher::EventPriority::Low>(
-        std::function<void(void)>(
-            [&](void) -> void {
-                if(!this->getState())
-                    return;
-                
-                auto instance = MC::getClientInstance();
-                auto lp = instance ? instance->getLocalPlayer() : nullptr;
-
-                if(!lp)
-                    return;
-            }
-        )
-    );
-
     this->registerEvent<EventType::Packet_SendToServer, EventDispatcher::EventPriority::Medium>(
         std::function<void(LoopbackPacketSender*, Packet*, bool*)>(
             [&](LoopbackPacketSender* pktSender, Packet* packet, bool* cancel) -> void {
