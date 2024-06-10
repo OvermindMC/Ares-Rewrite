282740bc86ec6940a47ef2fd29c0e2ff484e247e
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-06-10 18:49:12 +0100

    Update Hitbox.h

--- Client/Manager/Modules/Module/Combat/Hitbox.h
+++ Client/Manager/Modules/Module/Combat/Hitbox.h
@@ -6,6 +6,6 @@ class Hitbox : public Module {
 public:
     Hitbox(Manager*);
 public:
-    Vec2<float> radius;
+    Vec2<float> radius = Vec2<float>(0.6f, 1.8f);
     std::map<uint8_t, Vec2<float>> cached;
 };
