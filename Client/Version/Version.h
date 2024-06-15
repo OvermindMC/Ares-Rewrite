929a39869e53b2d8fe70f99e2fd24209a1d27138
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-06-15 09:49:53 +0100

    Update Version.h

--- Client/Version/Version.h
+++ Client/Version/Version.h
@@ -5,8 +5,21 @@
 class Version {
 public:
     Version(int major_version, int minor_version, int patch_version) : major_ver(major_version), minor_ver(minor_version), patch_ver(patch_version) {};
+    Version(const std::string& version_str) {
+        std::istringstream iss(version_str);
+        char dot;
+
+        iss >> major_ver >> dot >> minor_ver >> dot >> patch_ver;
+    };
 public:
     auto get(void) -> std::string;
+
+    auto operator>(const Version& other) const -> bool;
+    auto operator<(const Version& other) const -> bool;
+    auto operator>=(const Version& other) const -> bool;
+    auto operator<=(const Version& other) const -> bool;
+    auto operator==(const Version& other) const -> bool;
+    auto operator!=(const Version& other) const -> bool;
 private:
     int major_ver, minor_ver, patch_ver;
 };
