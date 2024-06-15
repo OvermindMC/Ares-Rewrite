9bd791087fb8193d4b9c38a15f0238b9484b13b8
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-06-15 07:19:37 +0100

    Update Socket.h

--- Client/Socket/Socket.h
+++ Client/Socket/Socket.h
@@ -16,4 +16,15 @@ private:
 public:
     SocketLayer(Client* client_ptr);
     ~SocketLayer(void);
+
+    auto tryConnect(void) -> void;
+
+    auto start(void) -> void;
+    auto stop(void) -> void;
+    auto run(void) -> void;
+
+    auto isConnected(void) -> bool;
+
+    std::thread socketThread;
+    std::atomic<bool> running;
 };
