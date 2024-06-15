8ef5fcd1904704b3de0d271f8ee603694f5cec42
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-06-15 09:49:55 +0100

    Update Version.cpp

--- Client/Version/Version.cpp
+++ Client/Version/Version.cpp
@@ -8,3 +8,43 @@ auto Version::get(void) -> std::string {
     return os.str();
 
 };
+
+auto Version::operator>(const Version& other) const -> bool {
+    if (major_ver > other.major_ver) {
+        return true;
+    } else if (major_ver < other.major_ver) {
+        return false;
+    };
+
+    if (minor_ver > other.minor_ver) {
+        return true;
+    } else if (minor_ver < other.minor_ver) {
+        return false;
+    };
+
+    if (patch_ver > other.patch_ver) {
+        return true;
+    };
+
+    return false;
+};
+
+auto Version::operator<(const Version& other) const -> bool {
+    return !(*this > other) && !(*this == other);
+};
+
+auto Version::operator>=(const Version& other) const -> bool {
+    return (*this > other) || (*this == other);
+};
+
+auto Version::operator<=(const Version& other) const -> bool {
+    return (*this < other) || (*this == other);
+};
+
+auto Version::operator==(const Version& other) const -> bool {
+    return major_ver == other.major_ver && minor_ver == other.minor_ver && patch_ver == other.patch_ver;
+};
+
+auto Version::operator!=(const Version& other) const -> bool {
+    return !(*this == other);
+};
