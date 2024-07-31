df8a105238e30809a0769085c4800edbd384761b
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-07-31 08:23:48 +0100

    Update Actor.cpp

--- SDK/Classes/Entity/Actor.cpp
+++ SDK/Classes/Entity/Actor.cpp
@@ -1,5 +1,21 @@
 #include "Actor.h"
 
-EntityContext* Actor::getEntityCtx() const {
-    return (EntityContext*)((char*)(this) + 0x8);
+Vec3 Actor::getPosition() const {
+    return this->getComponent<StateVectorComponent>()->position;
+};
+
+bool Actor::isJumping() const {
+    return this->hasComponent<FlagComponent<MobIsJumpingFlag>>();
+};
+
+bool Actor::isOnGround() const {
+    return this->hasComponent<OnGroundFlagComponent>();
+};
+
+void Actor::setIsOnGround(bool state) {
+    if(state && !this->isOnGround()) {
+        this->ctx.registry.emplace<OnGroundFlagComponent>(this->ctx.entityId);
+    } else if(!state && this->isOnGround()) {
+        this->removeComponent<OnGroundFlagComponent>();
+    };
 };
