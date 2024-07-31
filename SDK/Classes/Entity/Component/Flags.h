0f364fb433153fff433d01a6fb7bc5a0fd1049b3
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-07-31 08:23:33 +0100

    Create Flags.h

---
+++ SDK/Classes/Entity/Component/Flags.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "../../../../Mem/Mem.h"
+
+struct CollisionFlag {
+    //
+};
+
+struct VerticalCollisionFlag {
+    //
+};
+
+struct HorizontalCollisionFlag {
+    //
+};
+
+struct MobIsJumpingFlag {
+    //
+};
+
+struct JumpFromGroundRequestFlag {
+    //
+};
+
+struct WasOnGroundFlag {
+    //
+};
+
+struct PersistSitFlag {
+    //
+};
+
+struct GroundTravelFlag {
+    //
+};
+
+struct DashJump {
+    //
+};
+
+struct InWaterFlag {
+    //
+};
+
+struct InLavaFlag {
+    //
+};
+
+struct ActorIsImmobileFlag {
+    //
+};
+
+template<typename T>
+class FlagComponent {
+    //
+};
