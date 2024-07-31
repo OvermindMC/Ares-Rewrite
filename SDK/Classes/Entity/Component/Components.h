a6105a346da450e64ec8ce24201194cd9d1451db
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-07-31 08:23:38 +0100

    Update Components.h

--- SDK/Classes/Entity/Component/Components.h
+++ SDK/Classes/Entity/Component/Components.h
@@ -1,9 +1,25 @@
 #pragma once
 
-#include "../../../../Mem/Mem.h"
+#include "Flags.h"
+
+struct RuntimeIDComponent {
+    uint64_t runtimeId;
+};
 
 struct StateVectorComponent {
     Vec3 position;
     Vec3 oldPosition;
     Vec3 positionDelta;
+};
+
+struct OnGroundFlagComponent {
+    //
+};
+
+struct BlockMovementSlowdownMultiplierComponent {
+    //
+};
+
+struct ItemInUseComponent {
+    //
 };
