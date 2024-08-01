ada0b2867ebcddaa241e6d52a6c63210f8749b6a
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-08-01 20:17:48 +0100

    Update Module.h

--- Client/Manager/Modules/Module/Module.h
+++ Client/Manager/Modules/Module/Module.h
@@ -16,6 +16,21 @@ public:
     void setIsEnabled(bool);
     std::string getName() const;
 
+    template<EventType type, typename... Args>
+    void registerEvent(EventDispatcher::EventPriority priority, std::function<void(Args...)> fCallback) {
+        this->evDispatcher->registerEvent<type>(priority, fCallback);
+    };
+
+    template<EventType type>
+    void registerEvent(EventDispatcher::EventPriority priority, std::function<void()> fCallback) {
+        this->evDispatcher->registerEvent<type>(priority, fCallback);
+    };
+
+    template<EventType type>
+    void unregisterEvent() {
+        this->evDispatcher->unregisterEvent(type);
+    };
+
 private:
     Category* cPtr = nullptr;
     EventDispatcher* evDis;
