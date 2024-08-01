b935b68a5f9db1b2715d4e40ee8f8de01a13dd77
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-08-01 17:43:20 +0100

    Update EntityContext.h

--- SDK/Classes/Entity/EntityContext.h
+++ SDK/Classes/Entity/EntityContext.h
@@ -1,9 +1,46 @@
 #pragma once
 
-#include "../../../Mem/Mem.h"
+#include "Component/Components.h"
+
+struct EntityRegistry : std::enable_shared_from_this<EntityRegistry> {
+    std::string name;
+    entt::basic_registry<EntityId> registry;
+    uint32_t id;
+};
 
 struct EntityContext {
-    void* mRegistry;
-    entt::basic_registry<EntityId>& registry;
-    EntityId entityId;
+    EntityRegistry& registry;
+    entt::basic_registry<EntityId>& enttRegistry;
+    EntityId entity;
+
+    template<typename T>
+    T* tryGetComponent() {
+        return this->enttRegistry.try_get<T>(this->entity);
+    };
+
+    template<typename T>
+    const T* tryGetComponent() const {
+        return this->enttRegistry.try_get<T>(this->entity);
+    };
+
+    template<typename... T>
+    bool hasComponent() const {
+        if constexpr (sizeof...(T) > 0) {
+            return (this->enttRegistry.any_of<T>(this->entity) && ...);
+        } else {
+            return false;
+        };
+    };
+
+    template<typename T>
+    void addComponent() {
+        return this->enttRegistry.get_or_emplace<T>(this->entity);
+    };
+
+    template<typename... T>
+    void removeComponent() const {
+        (void)std::initializer_list<int>{
+            (this->enttRegistry.any_of<T>(this->entity) ? (this->enttRegistry.remove<T>(this->entity), 0) : 0)...
+        };
+    };
 };
