93ca16c2723c2c6a08d4f0cb6acfe22bf1e19ce3
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-08-01 17:49:02 +0100

    Update Level_Hook.cpp

--- Client/Manager/Hook/Hooks/Level/Level_Hook.cpp
+++ Client/Manager/Hook/Hooks/Level/Level_Hook.cpp
@@ -3,10 +3,7 @@
 Level_Hook::Level_Hook(Manager* mgr) : Hook<void, Level*>(mgr, "Level_Hook", mgr->getSig<void**>("Level_VTable")[99],
     [&](Level* level) -> void {
 
-        auto player = MC::getClientInstance()->getPlayer();
-
-        if(player)
-            player->setIsOnGround(true);
+        //
 
         return this->_Func(level);
     }
