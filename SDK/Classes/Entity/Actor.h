9203318771b35b3d0c6c15e02a862f9a85801d63
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-08-01 17:43:24 +0100

    Update Actor.h

--- SDK/Classes/Entity/Actor.h
+++ SDK/Classes/Entity/Actor.h
@@ -3,28 +3,13 @@
 #include "EntityContext.h"
 #include "Component/Components.h"
 
+#include "../Level/Level.h"
+
 class Actor {
 private:
     virtual ~Actor();
 public:
     EntityContext ctx;
-
-    template<typename T>
-    T* getComponent() const {
-        return ctx.registry.try_get<T>(ctx.entityId);
-    };
-
-    template<typename T>
-    bool hasComponent() const {
-        return ctx.registry.any_of<T>(ctx.entityId);
-    };
-
-    template<typename... T>
-    void removeComponent() const {
-        (void)std::initializer_list<int>{
-            (ctx.registry.any_of<T>(ctx.entityId) ? (ctx.registry.remove<T>(ctx.entityId), 0) : 0)...
-        };
-    };
 
     Vec3 getPosition() const;
     
