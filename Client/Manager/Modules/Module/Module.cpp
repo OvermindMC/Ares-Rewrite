5786a8859307a80f2baaf8abee945ef70145fbc4
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-08-01 21:07:52 +0100

    Update Module.cpp

--- Client/Manager/Modules/Module/Module.cpp
+++ Client/Manager/Modules/Module/Module.cpp
@@ -1,34 +1,34 @@
 #include "Module.h"
 
-Module::Module(Category* category, const std::string& display_name) : cPtr(category), evDis(new EventDispatcher()), name(display_name) {
+Module::Module(Category* category, const std::string& display_name) : cPtr(category), evDis(std::make_unique<EventDispatcher>()), name(display_name) {
     category->registerModule(this);
 
-    this->evDispatcher->registerEvent<EventType::BaseTick>(
+    this->evDis->registerEvent<EventType::BaseTick>(
         EventDispatcher::EventPriority::High,
         [&]() {
             if(this->state.first != this->state.second) {
                 this->state.second = this->state.first;
 
                 if(this->state.first) {
-                    this->evDispatcher->dispatchEvent<EventType::OnEnable>();
+                    this->evDis->dispatchEvent<EventType::OnEnable>();
                 } else {
-                    this->evDispatcher->dispatchEvent<EventType::OnDisable>();
+                    this->evDis->dispatchEvent<EventType::OnDisable>();
                 };
             };
 
             if(this->state.first) {
-                this->evDispatcher->dispatchEvent<EventType::OnTick>();
+                this->evDis->dispatchEvent<EventType::OnTick>();
             };
         }
     );
 };
 
-Module::~Module() {
-    delete this->evDispatcher;
-};
-
 Manager* Module::getMgr() const {
     return this->category ? this->category->mgr : nullptr;
+};
+
+EventDispatcher* Module::getEventDispatcher() const {
+    return this->evDis.get();
 };
 
 bool Module::isEnabled() const {
