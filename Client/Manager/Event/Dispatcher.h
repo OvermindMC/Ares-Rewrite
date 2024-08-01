410c97ce8b6e906b6c580631a33ee5c1a3739b9f
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-08-01 21:07:59 +0100

    Update Dispatcher.h

--- Client/Manager/Event/Dispatcher.h
+++ Client/Manager/Event/Dispatcher.h
@@ -2,11 +2,32 @@
 
 #include "Types.h"
 
+class BaseEvent {
+public:
+    virtual ~BaseEvent() = default;
+    virtual void invoke(void* args) const = 0;
+    virtual std::unique_ptr<BaseEvent> clone() const = 0;
+};
+
 template<typename... Args>
-class Event {
+class Event : public BaseEvent {
 public:
     std::function<void(Args...)> callback;
-    Event(std::function<void(Args...)> fn) : callback(fn) {};
+
+    Event(std::function<void(Args...)> fn) : callback(std::move(fn)) {}
+
+    void invoke(void* args) const override {
+        call(std::index_sequence_for<Args...>{}, args);
+    };
+
+    std::unique_ptr<BaseEvent> clone() const override {
+        return std::make_unique<Event<Args...>>(*this);
+    };
+private:
+    template<std::size_t... Is>
+    void call(std::index_sequence<Is...>, void* args) const {
+        callback(std::get<Is>(*reinterpret_cast<std::tuple<Args...>*>(args))...);
+    };
 };
 
 class EventDispatcher {
@@ -15,59 +36,41 @@ public:
 
     template<EventType type, typename... Args>
     void registerEvent(EventPriority priority, std::function<void(Args...)> fCallback) {
-        this->events_map[type].emplace_back(priority, new Event<Args...>(fCallback));
+        events_map[type].emplace_back(priority, std::make_unique<Event<Args...>>(std::move(fCallback)));
     };
-    
+
     template<EventType type>
     void registerEvent(EventPriority priority, std::function<void()> fCallback) {
-        this->events_map[type].emplace_back(priority, new Event<>(fCallback));
+        events_map[type].emplace_back(priority, std::make_unique<Event<>>(std::move(fCallback)));
     };
-    
+
     void unregisterEvent(EventType type) {
-        auto iter = this->events_map.find(type);
-
-        if(iter != this->events_map.end()) {
-            auto& list = iter->second;
-
-            for(auto& event : list) {
-                if(event.second) {
-                    delete static_cast<Event<>*>(event.second);
-                    event.second = nullptr;
-                };
-            };
-
-            events_map.erase(iter->first);
-        };
+        events_map.erase(type);
     };
 
     template<EventType type, typename... Args>
     void dispatchEvent(Args&&... args) {
-        auto iter = this->events_map.find(type);
-
-        if(iter != this->events_map.end()) {
+        auto iter = events_map.find(type);
+        if (iter != events_map.end()) {
             auto& list = iter->second;
-
             std::sort(list.begin(), list.end(), [](const auto& a, const auto& b) {
                 return a.first > b.first;
             });
 
-            for(auto& event : list) {
-                if(event.second) {
-                    static_cast<Event<Args...>*>(event.second)->callback(args...);
+            std::tuple<Args...> arguments(std::forward<Args>(args)...);
+            for (const auto& event : list) {
+                if (event.second) {
+                    static_cast<const Event<Args...>*>(event.second.get())->invoke(&arguments);
                 };
             };
         };
     };
 
-    std::map<EventType, std::vector<std::pair<EventDispatcher::EventPriority, void*>>> getEvents() const {
-        return this->events_map;
+    std::map<EventType, std::vector<std::pair<EventPriority, std::unique_ptr<BaseEvent>>>> const& getEvents() const {
+        return events_map;
     };
 
-    ~EventDispatcher(void) {
-        for(auto& event : this->events_map) {
-            this->unregisterEvent(event.first);
-        };
-    };
+    ~EventDispatcher() = default;
 private:
-    std::map<EventType, std::vector<std::pair<EventDispatcher::EventPriority, void*>>> events_map;
+    std::map<EventType, std::vector<std::pair<EventPriority, std::unique_ptr<BaseEvent>>>> events_map;
 };
