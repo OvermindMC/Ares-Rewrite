c9e9de12b8b6f8bf97adf7bdf076f07054ec4b23
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-08-01 17:43:41 +0100

    Update EnTT.h

--- Mem/EnTT/EnTT.h
+++ Mem/EnTT/EnTT.h
@@ -1,22 +1,35 @@
 #pragma once
 
-#include <cstdint>
-
 #include <entt/entity/entity.hpp>
 
-enum class EntityId : std::uint32_t {};
+struct EntityId {
+    std::uint32_t rawId;
 
-template<>
-struct entt::internal::entt_traits<EntityId> {
+    [[nodiscard]] constexpr bool operator==(const EntityId& other) const = default;
+
+    [[nodiscard]] constexpr operator std::uint32_t() const {
+        return this->rawId;
+    }
+};
+
+struct EntityIdTraits {
     using value_type = EntityId;
 
     using entity_type = std::uint32_t;
     using version_type = std::uint16_t;
 
-    static constexpr entity_type entity_mask = 0x3FFFF;
-    static constexpr entity_type version_mask = 0x3FFF;
+    static constexpr entity_type entity_mask = 0x3FFFF; // lower 18 bits of raw id
+    static constexpr entity_type version_mask = 0x3FFF; // upper 14 bits of raw id
 };
 
-#undef max
-#include <entt/entt.hpp>
+template<>
+struct entt::entt_traits<EntityId> : entt::basic_entt_traits<EntityIdTraits> {
+    static constexpr std::size_t page_size = 2048;
+};
+
+template<typename Type>
+struct entt::storage_type<Type, EntityId> {
+    using type = basic_storage<Type, EntityId>;
+};
+
 #include <entt/entity/registry.hpp>
