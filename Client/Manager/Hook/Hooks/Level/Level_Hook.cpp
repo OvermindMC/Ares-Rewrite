55c4c82dbaacce2d3ad7a80510db3b90489d38d6
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-08-01 20:17:39 +0100

    Update Level_Hook.cpp

--- Client/Manager/Hook/Hooks/Level/Level_Hook.cpp
+++ Client/Manager/Hook/Hooks/Level/Level_Hook.cpp
@@ -2,9 +2,7 @@
 
 Level_Hook::Level_Hook(Manager* mgr) : Hook<void, Level*>(mgr, "Level_Hook", mgr->getSig<void**>("Level_VTable")[99],
     [&](Level* level) -> void {
-
-        //
-
+        this->mgr->dispatchEvent<EventType::OnLevel>(level);
         return this->_Func(level);
     }
 ){};
