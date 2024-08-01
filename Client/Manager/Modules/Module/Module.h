ad2e5d77f69ccadb30aa1cfc92fa0d3ac1dc69da
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-08-01 21:07:50 +0100

    Update Module.h

--- Client/Manager/Modules/Module/Module.h
+++ Client/Manager/Modules/Module/Module.h
@@ -5,12 +5,12 @@
 class Module {
 public:
     Module(Category*, const std::string&);
-    virtual ~Module();
+    virtual ~Module() = default;
 
     PTR_ACCESS(Category*, category, cPtr);
-    PTR_ACCESS(EventDispatcher*, evDispatcher, evDis);
 
     Manager* getMgr() const;
+    EventDispatcher* getEventDispatcher() const;
 
     bool isEnabled() const;
     void setIsEnabled(bool);
@@ -18,22 +18,22 @@ public:
 
     template<EventType type, typename... Args>
     void registerEvent(EventDispatcher::EventPriority priority, std::function<void(Args...)> fCallback) {
-        this->evDispatcher->registerEvent<type>(priority, fCallback);
+        this->evDis->registerEvent<type>(priority, fCallback);
     };
 
     template<EventType type>
     void registerEvent(EventDispatcher::EventPriority priority, std::function<void()> fCallback) {
-        this->evDispatcher->registerEvent<type>(priority, fCallback);
+        this->evDis->registerEvent<type>(priority, fCallback);
     };
 
     template<EventType type>
     void unregisterEvent() {
-        this->evDispatcher->unregisterEvent(type);
+        this->evDis->unregisterEvent(type);
     };
 
 private:
     Category* cPtr = nullptr;
-    EventDispatcher* evDis;
+    std::unique_ptr<EventDispatcher> evDis;
     
     const std::string name;
     std::pair<bool, bool> state = {false, false};
