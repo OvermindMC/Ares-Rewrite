30bdf830170652f564788854c41227a37fce7ab4
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-08-04 13:10:29 +0100

    Update GmTick_Hook.cpp

--- Client/Manager/Hook/Hooks/GameMode/GmTick_Hook.cpp
+++ Client/Manager/Hook/Hooks/GameMode/GmTick_Hook.cpp
@@ -1,7 +1,7 @@
 #include "GmTick_Hook.h"
 
-GmTick_Hook::GmTick_Hook(Manager* mgr) : Hook<void, void*>(mgr, "GmTick_Hook", mgr->getSig<void**>("GameMode_VTable")[8],
-    [&](void* GM) -> void {
+GmTick_Hook::GmTick_Hook(Manager* mgr) : Hook<void, GameMode*>(mgr, "GmTick_Hook", mgr->getSig<void**>("GameMode_VTable")[8],
+    [&](GameMode* GM) -> void {
         this->mgr->dispatchEvent<EventType::OnGameMode>(GM);
         return this->_Func(GM);
     }
