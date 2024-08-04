7460d6b6901aae9a1cf9851d79df0120d640bc82
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-08-04 13:09:49 +0100

    Update Player.h

--- SDK/Classes/Entity/Player.h
+++ SDK/Classes/Entity/Player.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #include "Actor.h"
+#include "../GameMode.h"
 
 class Player : public Actor {
 private:
     virtual ~Player();
 public:
+    void swing();
     void setSprinting(bool);
     void displayClientMsg(std::string);
+
+    GameMode* getGameMode();
 };
