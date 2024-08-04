30f63b8f976c7e8025c00f9572f4c8352dee7963
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-08-04 01:35:02 +0100

    Update Player.cpp

--- SDK/Classes/Entity/Player.cpp
+++ SDK/Classes/Entity/Player.cpp
@@ -8,3 +8,12 @@ void Player::setSprinting(bool state) {
     if(func)
         func(this, state);
 };
+
+void Player::displayClientMsg(std::string message) {
+    static void* sig = Mem::getSig("48 89 5C 24 ? 48 89 74 24 ? 57 48 83 EC 40 48 8B F2 48 8B 89 ? ? ? ?"); // vtable index[213] - 1.21.2
+    using Func = void (__thiscall*)(Player*, std::string*);
+    Func func = (Func)sig;
+
+    if(func)
+        func(this, &message);
+};
