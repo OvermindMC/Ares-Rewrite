302098acd691bcb01046eb99d7366711247f311d
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-08-06 08:14:39 +0100

    Update AirJump.cpp

--- Client/Manager/Modules/Module/Movement/AirJump.cpp
+++ Client/Manager/Modules/Module/Movement/AirJump.cpp
@@ -1,11 +1,12 @@
 #include "AirJump.h"
 
 AirJump::AirJump(Category* c) : Module(c, "AirJump") {
-    this->setIsEnabled(true);
-
     this->registerEvent<EventType::OnLevel, Level*>(
         EventDispatcher::EventPriority::Medium,
         [&](Level* level) {
+            if(!this->isEnabled())
+                return;
+            
             auto lp = MC::getClientInstance()->getPlayer();
 
             if(lp)
