52b95db2455c0bff43e0b5f36e348cd3f42d4c8e
OvermindMC <66936044+OvermindMC@users.noreply.github.com>
2024-08-06 08:42:38 +0100

    Update TestModule.cpp

--- Client/Manager/Modules/Module/Misc/TestModule.cpp
+++ Client/Manager/Modules/Module/Misc/TestModule.cpp
@@ -3,17 +3,48 @@
 TestMod::TestMod(Category* c) : Module(c, "Test Module") {
     this->setIsEnabled(true);
     
+    this->registerEvent<EventType::OnEnable>(
+        EventDispatcher::EventPriority::Low,
+        [&]() {
+            //
+        }
+    );
+
+    this->registerEvent<EventType::OnDisable>(
+        EventDispatcher::EventPriority::Low,
+        [&]() {
+            //
+        }
+    );
+    
     this->registerEvent<EventType::OnTick>(
-        EventDispatcher::EventPriority::High,
+        EventDispatcher::EventPriority::Low,
         [&]() {
             //
         }
     );
 
     this->registerEvent<EventType::OnLevel, Level*>(
-        EventDispatcher::EventPriority::Medium,
+        EventDispatcher::EventPriority::Low,
         [&](Level* level) {
-            //
+            if(!this->isEnabled())
+                return;
+        }
+    );
+
+    this->registerEvent<EventType::OnGameMode, GameMode*>(
+        EventDispatcher::EventPriority::Low,
+        [&](GameMode* GM) {
+            if(!this->isEnabled())
+                return;
+        }
+    );
+
+    this->registerEvent<EventType::OnFirstPersonLook, Vec2*>(
+        EventDispatcher::EventPriority::Low,
+        [&](Vec2* mAngles) {
+            if(!this->isEnabled())
+                return;
         }
     );
 };
